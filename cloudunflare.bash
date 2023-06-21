38016a8dda59bcab16a1be59c06e498173c9905d
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2023-06-21 14:55:05 +0630

    Update cloudunflare.bash
    account

--- cloudunflare.bash
+++ cloudunflare.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # CloudUnflare
 
-CompleteDNS_Login='email@mail.com|password'
+CompleteDNS_Login='freevpn537@gmail.com|sithu@244804'
 
 if [[ -z $(command -v dig) ]]; then
 	echo " ERROR: \"dig\" command not found"
